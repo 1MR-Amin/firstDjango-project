f112357af38b2806bb2f0d7375365a3a6fa8e1d3
root <root@DESKTOP-OAE4C2M.localdomain>
2021-11-23 20:12:52 +0300

    project initialized

--- firstDjango-project/firstApp/models.py
+++ firstDjango-project/firstApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    gender = models.BooleanField()
+
+    def __str__(self):
+        if self.gender:
+            return f"{self.first_name} {self.last_name} M"
+        return f"{self.first_name} {self.last_name} F"
+
+
+class Contact(models.Model):
+    telephone = models.CharField(max_length=50 ,null=False)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        if self.email:
+            return self.email
+        return self.telephone
